5be0a63f46be85a05d41d8321343885f39ee8a9a
Jacob Jude <jacobj.jude@outlook.com>
2022-06-22 11:39:03 -0400

    made a random word generator

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+let alphabet = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z'
+];
+
+function randNum() {
+	return Math.floor(Math.random() * (26 - 1 + 1) + 1);
+}
+
+function formRandWord() {
+	let htmlString = '';
+	for (i = 0; i < 7; i++) {
+		let j = randNum();
+		debugger;
+		htmlString += alphabet[j];
+	}
+	return htmlString;
+}
+
+let word = formRandWord();
+const parentHTMLElement = document.querySelector('.randWord');
+parentHTMLElement.innerHTML += word;
